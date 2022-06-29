9a48bd721c49f6af38e743bb8e376bea72deb663
root <root@DESKTOP-6B3FQL8.localdomain>
2022-06-29 09:44:28 -0500

    added simple background color generator and more comments

---
+++ apps.js
@@ -0,0 +1,35 @@
+'use strict'
+console.log('working');
+
+// making an array with color names
+const bgColors = ["Lightblue", "Blue", "Orange", "Yellow", "Purple", "Grey", "Lightblue", "Green", "Yellowgreen","Pink","Red", "Lawngreen","Lightcoral"];
+
+// grabbing the .color id from the html doc
+const color = document.querySelector(".color");
+
+// grabbing the generate id from the html doc
+const generate = document.getElementById("generate");
+
+
+// make a function that randomize the colors
+function getRandomBgColor(){
+
+    // randomizes the colors
+ return Math.floor(Math.random() * bgColors.length);
+
+}
+
+// make the button click and change background color
+generate.addEventListener("click",function(){
+    
+    // let a name equal the function above so onclick it calls it
+    let randomBgColor = getRandomBgColor();
+    console.log(randomBgColor);
+    
+
+//make the backgroud color = what ever the rgb numbers of bgColors
+    document.body.style.background = bgColors[randomBgColor]
+
+    //write the color on html doc for the user to see
+    color.textContent = bgColors[randomBgColor];
+})
